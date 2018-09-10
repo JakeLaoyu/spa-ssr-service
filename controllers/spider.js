ad8178ac10c9fb28a45c971386bbc29a8a98a0f9
Jake <Jake.Laoyu@gmail.com>
2018-09-10 11:51:47 +0800

    feat: Identify mobile user-agent

--- controllers/spider.js
+++ controllers/spider.js
@@ -12,7 +12,7 @@ class Spider {
   async index (ctx) {
     // console.log(ctx)
     const page = await browser.newPage()
-    // await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1')
+    await page.setUserAgent(ctx.request.header['user-agent'])
     await page.goto(`${Config.site}${ctx.request.url}`)
     await page.waitFor(1000)
     await page.evaluate(() => {
